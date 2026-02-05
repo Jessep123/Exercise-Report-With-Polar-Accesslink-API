50a0362ef286b364f1c0297c061745118620c9dc
Jesse Pilcher <jesse.pilcher@orionhealth.com>
2026-02-05 14:59:13 +1300

    Updated 00_api_data_load to pull data from polar, compare to whats in a seperate online neon database and then update neon database accordingly.

--- 00_api_data_load.py
+++ 00_api_data_load.py
@@ -5,53 +5,71 @@ Polar AccessLink API only provides data uploaded to Polar Flow within the last 3
 
 This creates a redundancy as data from more than a month ago cannot be accessed
 
-To avoid this I've developed the 00_api_data_load.py script, which will pull and upload exercise data into a seperate dataset 
+To avoid this I've developed the 00_api_data_load.py script, which will pull and upload exercise data into a Neon PostGres SQL server 
 
-This dataset will be updated whenever this script is run, adding new instances in
+This online dataset will be updated whenever this script is run, adding new instances in
 
 For now it will just handle the loading of data, processing will be added in another file for compartmentalization
 '''
 
+#Needed to import data from polar API
 import requests
 import os
-user_id = os.environ.get("POLAR_USER_ID")
+import pandas as pd
+
+#Secrets/access tokens required for script
 access_token = os.environ.get("POLAR_ACCESS_TOKEN")
-client_id = os.environ.get("POLAR_CLIENT_ID")
-client_secret_id = os.environ.get("POLAR_CLIENT_SECRET")
+neon_url = os.environ["NEON_DATABASE_URL"]
 
-
-#Defining API headers and parameters to pull data
-header = {
+#Defining header/params for accessing polar data
+exercises_header = {
   'Accept': 'application/json',  
   'Authorization': f'Bearer {access_token}'
 }
-
 params = {'zones': 'True',
           'samples':'True'}
 
+#Importing data from polar API
 exercise_data = requests.get(f'https://www.polaraccesslink.com/v3/exercises',
-                              headers = header,
+                              headers = exercises_header,
                               params = params
                               ).json()
 
-#Loading data as dataframe with id column as index
-import pandas as pd
-dataframe = pd.DataFrame(exercise_data)
+polar_data = pd.DataFrame(exercise_data)
 
-#Path to load dataset
-data_path = r'C:\Users\jessep.LAPTOP-7GPOPVDF\Desktop\Personal Projects\polar_data.csv'
 
-#Try loading data from csv path, if it fails, create a csv
-try:
-    polar_data = pd.read_csv(data_path)
+#Importing neon dataset
+from sqlalchemy import create_engine, text
+import psycopg2
 
-except:
-    print('Creating data csv')
-    dataframe.to_csv(data_path, index = False)
-    quit()
+engine = create_engine(neon_url)
 
-#Append any new data to the polar data csv
-polar_data = pd.concat([polar_data, dataframe]).drop_duplicates(subset = ['id'])
+#SQL Query
+query = """
+SELECT *
+FROM polar_data
+"""
+neon_data = pd.read_sql(text(query), engine)
 
-#Saving csv 
-polar_data.to_csv(data_path, index=False)
+#Isolate any new rows from the polar API
+new_data = polar_data[~polar_data["id"].isin(neon_data["id"])]
+
+#Exporting data back to neon if there are new rows
+if len(new_data) != 0:
+
+    engine.dispose()  # closes old pooled connections
+    engine = create_engine(os.environ["NEON_DATABASE_URL"])
+    #Converting columns to text for simplicities sake
+    for col in new_data.columns:
+        new_data[col] = new_data[col].where(new_data[col].isna(), new_data[col].astype(str))
+        
+    new_data.to_sql(
+        "polar_data",
+        engine,
+        if_exists="append",
+        index=False,
+        method="multi",
+        chunksize=1000,
+    )
+
+print(f"Uploaded {len(new_data)} rows")
